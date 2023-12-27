75b2588d8544ebb96149df79fd89fd12a6bde198
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2023-12-27 13:24:51 +0200

    craete recipe model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShopingEditComponent } from './shopping-list/shoping-edit/shoping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RecipesComponent,
-    ShoppingListComponent
+    ShoppingListComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    ShopingEditComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/recipes/recipes.model.ts
@@ -0,0 +1,8 @@
+// Because will use to define how data will be
+export class Recipe {
+  constructor(
+    public name: string,
+    public decripe: string,
+    public imagePath: string
+  ) {}
+}
