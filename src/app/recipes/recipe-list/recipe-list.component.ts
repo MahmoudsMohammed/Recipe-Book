adc46cb75a21273734951db71386bed48c7e9245
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2023-12-27 13:54:27 +0200

    conect components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.css'
+  styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    {
+      name: 'A Test Recipe',
+      decripe: 'This is Simply a Test',
+      imagePath: '../../../assets/recipe.jpg',
+    },
+    {
+      name: 'A Test Recipe',
+      decripe: 'This is Simply a Test',
+      imagePath: '../../../assets/recipe.jpg',
+    },
+  ];
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { Ingredient } from '../shared/ingredient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrl: './shopping-list.component.css'
+  styleUrl: './shopping-list.component.css',
 })
 export class ShoppingListComponent {
-
+  ingredients: Ingredient[] = [
+    { name: 'Apple', amount: 5 },
+    { name: 'Tomato', amount: 7 },
+  ];
 }
