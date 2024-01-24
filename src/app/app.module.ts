53f5c51b468900c12870e0f6d421f34be93ff870
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2024-01-24 09:31:53 +0200

    create core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { authComponent } from './auth/auth.component';
-import { authInterceptor } from './auth/auth.interceptor.service';
 import { recipeModule } from './recipes/recipe.module';
 import { shoppingModule } from './shopping-list/shopping.module';
 import { sharedModule } from './Shared/shared.module';
+import { coreModule } from './core.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, authComponent],
@@ -23,9 +23,7 @@ import { sharedModule } from './Shared/shared.module';
     recipeModule,
     shoppingModule,
     sharedModule,
-  ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: authInterceptor, multi: true },
+    coreModule,
   ],
   bootstrap: [AppComponent],
 })
