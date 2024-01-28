3785696a83be8401d2593bff20d9359a234d8949
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2024-01-28 10:58:11 +0200

    save shopping list data

--- src/app/Shared/http.service.ts
+++ src/app/Shared/http.service.ts
@@ -2,15 +2,20 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { recipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipes.model';
-import {  map, tap } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
+import { shoppingService } from '../shopping-list/shopping.service';
+import { Ingredient } from '../Models/ingredient.model';
 
 @Injectable({ providedIn: 'root' })
 export class httpService {
   constructor(
     private http: HttpClient,
     private recipeServ: recipeService,
+    private shoppingServ: shoppingService
   ) {}
+  // sent data
   sendData() {
+    // sent recipes Data
     this.http
       .put<Recipe[]>(
         'https://recipe-book-4a739-default-rtdb.firebaseio.com/recipe.json',
@@ -19,7 +24,18 @@ export class httpService {
       .subscribe((res) => {
         console.log(res);
       });
+    // sent shopping list Data
+    this.http
+      .put<Ingredient[]>(
+        'https://recipe-book-4a739-default-rtdb.firebaseio.com/shopping.json',
+        this.shoppingServ.getIngredients
+      )
+      .subscribe((res) => {
+        console.log(res);
+      });
   }
+
+  // fetch recipes Data
   fetchData() {
     return this.http
       .get<Recipe[]>(
